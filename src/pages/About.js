66cbf1b237bffb8da60db56b0e1bc8da58ad7fbd
Famila-M <familafami99@outlook.com>
2021-07-27 09:49:32 +0530

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+      Our home page offers a user-friendly and topical approach and leads you to the 
+      product range, with search facilities, and with encyclopaedic brand and product 
+      information.We always have special offers and exclusive products.Our services 
+      continue to be utilised by tens of thousands of consumers, on/off trade accounts 
+      and corporate clients.
+      </p>
+    </section>
+  );
+}
